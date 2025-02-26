78aab04707dbc80f4a1148fe03a8ac588facaf3b
Lucas Yago <lucasyago63@gmail.com>
2025-02-26 19:23:21 -0300

    implementa funçao para adicionar amigos a lista

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-
 let amigos = []
+
+function adicionarAmigo(){
+    let novoAmigo = document.querySelector('input').value
+    if(novoAmigo == ''){
+        alert('Por favor, insira um nome válido.')
+    }else{
+        amigos.push(novoAmigo)
+    }
+    limparCampo()
+}
+
+
+function limparCampo(){
+    document.querySelector('input').value = ''
+}
