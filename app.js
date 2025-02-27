9e41ef0bbd9969035cd7589e2dc358989d27f17f
Lucas Yago <lucasyago63@gmail.com>
2025-02-26 21:37:24 -0300

    implementa funçao de listagem de amigos na tela

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigos = []
+let amigos = [];
 
 function adicionarAmigo(){
-    let novoAmigo = document.querySelector('input').value
+    let novoAmigo = document.querySelector('input').value;
     if(novoAmigo == ''){
-        alert('Por favor, insira um nome válido.')
+        alert('Por favor, insira um nome válido.');
     }else{
-        amigos.push(novoAmigo)
+        amigos.push(novoAmigo);
+        console.log(amigos)
+        listaAmigos(amigos)
     }
-    limparCampo()
+    limparCampo();
 }
 
-
 function limparCampo(){
-    document.querySelector('input').value = ''
+    document.querySelector('input').value = '';
+}
+
+function listaAmigos(amigos){
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ""
+
+    for(let i = 0; i < amigos.length; i++){
+        let li = document.createElement('li');
+        li.textContent = amigos[i]
+        listaAmigos.appendChild(li)
+    }
 }
