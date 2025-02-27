23b6e5d34a078a722e8e72ca66549ff78b129c14
Lucas Yago <lucasyago63@gmail.com>
2025-02-27 02:51:37 -0300

    implementa funçao para sortear um amigo secreto aleatoriamente

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ function adicionarAmigo(){
         alert('Por favor, insira um nome válido.');
     }else{
         amigos.push(novoAmigo);
-        console.log(amigos)
-        listaAmigos(amigos)
+        listaAmigos(amigos);
     }
     limparCampo();
 }
@@ -19,11 +18,35 @@ function limparCampo(){
 
 function listaAmigos(amigos){
     let listaAmigos = document.getElementById('listaAmigos');
-    listaAmigos.innerHTML = ""
+    listaAmigos.innerHTML = "";
 
     for(let i = 0; i < amigos.length; i++){
         let li = document.createElement('li');
-        li.textContent = amigos[i]
-        listaAmigos.appendChild(li)
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
     }
+}
+
+function amigoAleatorio(listaDeAmigos){
+    return Math.floor(Math.random() * listaDeAmigos.length);
+}
+
+function sortearAmigo(){
+    let amigoSorteado = document.getElementById('resultado');
+    amigoSorteado.innerHTML = '';
+    
+    if(amigos.length > 0){
+        let li = document.createElement('li');
+        let amigoIndice = amigoAleatorio(amigos);
+        li.textContent = `O amigo secreto sorteado é: ${amigos[amigoIndice]}`;
+        limparListaAmigos();
+        amigoSorteado.appendChild(li);
+    }else{
+        alert('Não existem amigos disponíveis para serem sorteados.');
+    }
+}
+
+function limparListaAmigos(){
+    document.getElementById('listaAmigos').innerHTML = '';
+    amigos = [];
 }
